be4019e58d5020c85dd1df896ea0df8f3959e4af
agustinellargo <agustinellargo@gmail.com>
2024-08-01 22:37:45 +0200

    funcionando los IDs

--- agustin/src/main/java/com/example/agustin/controllers/AlumnoController.java
+++ agustin/src/main/java/com/example/agustin/controllers/AlumnoController.java
@@ -3,8 +3,9 @@ package com.example.agustin.controllers;
 import com.example.agustin.models.Alumno;
 import com.example.agustin.service.AlumnoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AlumnoController {
@@ -20,9 +21,21 @@ public class AlumnoController {
         return this.alumnoService.getAlumno();
 
     }
-    @GetMapping("/crearUsuario")
-    public Alumno crearUsuario(){
-        return this.tamagotchiService.crearTamagotchi();
-    }
+    @GetMapping("/crearUsuario/")
+    public Alumno crearUsuario(@RequestParam String nombre,
+                               @RequestParam int edad,
+                               @RequestParam int curso,
+                               @RequestParam String ciudad) {
 
+
+        return alumnoService.crearUsuario(nombre,edad,curso,ciudad);
+    }
+    @GetMapping("/usuarioRandom/{nombre}")
+    public Alumno usuarioRandom(@PathVariable String nombre){
+        return alumnoService.usuarioRandom(nombre);
+    }
+    @GetMapping("/listado/")
+    public List<Alumno> getlistado(){
+        return alumnoService.listaAlumnos();
+    }
 }
--- agustin/src/main/java/com/example/agustin/service/AlumnoService.java
+++ agustin/src/main/java/com/example/agustin/service/AlumnoService.java
@@ -4,10 +4,13 @@ import com.example.agustin.models.Alumno;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AlumnoService {
     private Alumno alumno;
-
+    List<Alumno> listado = new ArrayList<>();
 
     @Autowired
     public AlumnoService(Alumno alumno){
@@ -16,7 +19,28 @@ public class AlumnoService {
 
 
     public Alumno getAlumno(){
+
         return alumno;
     }
-
+    public Alumno usuarioRandom(String nombre){
+        Alumno alumno = new Alumno();
+        alumno.setNombre(nombre);
+        alumno.setCiudad("trebujena");
+        alumno.setEdad(28);
+        alumno.setCurso(6);
+        listado.add(alumno);
+        return alumno;
+    }
+    public Alumno crearUsuario(String nombre,int edad,int curso,String ciudad){
+        Alumno alumno = new Alumno();
+        alumno.setNombre(nombre);
+        alumno.setEdad(edad);
+        alumno.setCurso(curso);
+        alumno.setCiudad(ciudad);
+        listado.add(alumno);
+        return alumno;
+    }
+    public List<Alumno> listaAlumnos(){
+        return listado;
+    }
 }
